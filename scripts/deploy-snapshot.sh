22a360d971e0d1fd7cfffd09bdb69d28bd5cefb1
Alexander Grahn <agrahn@users.noreply.github.com>
2026-02-05 13:33:00 +0100

    updated copyright headers

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright © 2014-2021 The Android Password Store Authors. All Rights Reserved.
+# Copyright © 2014-2026 The Android Password Store Authors. All Rights Reserved.
 # SPDX-License-Identifier: GPL-3.0-only
 #
 
